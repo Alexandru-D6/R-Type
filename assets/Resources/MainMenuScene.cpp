d4f97a2afe923b32549068e7973e795230e14dad
Alexandru Dumitru <alexandru666@outlook.es>
2022-10-12 16:57:43 +0200

    creating the clases

---
+++ assets/Resources/MainMenuScene.cpp
@@ -0,0 +1,85 @@
+#include "GameScene.h"
+#include "Game.h"
+#include "GeneralDefines.h"
+
+GameScene::GameScene() {
+    map = NULL;
+    player = NULL;
+}
+
+GameScene::~GameScene() {
+    if(map != NULL)
+        delete map;
+    if(player != NULL)
+        delete player;
+}
+
+
+void GameScene::init() {
+    // 256.0f is the amount of pixel that has the map as height, it may need a rework to get that value directly from level.txt
+    projection = glm::ortho(0.f, float((SCREEN_WIDTH / float(SCREEN_HEIGHT / 256.0f)) - 1), float((SCREEN_HEIGHT / float(SCREEN_HEIGHT / 256.0f)) - 1), 0.f);
+    currentTime = 0.0f;
+
+    initShaders();
+    map = TileMap::createTileMap("levels/level00.txt", glm::vec2(SCREEN_X, SCREEN_Y), texProgram, projection);
+    player = new Player(projection);
+    player->init(glm::ivec2(SCREEN_X, SCREEN_Y), texProgram);
+    player->setPosition(glm::vec2(INIT_PLAYER_X_TILES * map->getTileSize(), INIT_PLAYER_Y_TILES * map->getTileSize()));
+    player->setTileMap(map);
+}
+
+void GameScene::update(int deltaTime) {
+    currentTime += deltaTime;
+    player->update(deltaTime);
+}
+
+void GameScene::render() {
+    glm::mat4 modelview;
+
+    texProgram.use();
+    texProgram.setUniformMatrix4f("projection", projection);
+    texProgram.setUniform4f("color", 1.0f, 1.0f, 1.0f, 1.0f);
+    modelview = glm::mat4(1.0f);
+    texProgram.setUniformMatrix4f("modelview", modelview);
+    texProgram.setUniform2f("texCoordDispl", 0.f, 0.f);
+
+    map->render();
+
+    texProgram.use();
+    texProgram.setUniformMatrix4f("projection", projection);
+    texProgram.setUniform4f("color", 1.0f, 1.0f, 1.0f, 1.0f);
+    modelview = glm::mat4(1.0f);
+    texProgram.setUniformMatrix4f("modelview", modelview);
+    texProgram.setUniform2f("texCoordDispl", 0.f, 0.f);
+
+    player->render();
+}
+
+void GameScene::initShaders() {
+    Shader vShader, fShader;
+
+    vShader.initFromFile(VERTEX_SHADER, "shaders/level-obstacles.vert");
+    if(!vShader.isCompiled())
+    {
+        cout << "Vertex Shader Error" << endl;
+        cout << "" << vShader.log() << endl << endl;
+    }
+    fShader.initFromFile(FRAGMENT_SHADER, "shaders/level-obstacles.frag");
+    if(!fShader.isCompiled())
+    {
+        cout << "Fragment Shader Error" << endl;
+        cout << "" << fShader.log() << endl << endl;
+    }
+    texProgram.init();
+    texProgram.addShader(vShader);
+    texProgram.addShader(fShader);
+    texProgram.link();
+    if(!texProgram.isLinked())
+    {
+        cout << "Shader Linking Error" << endl;
+        cout << "" << texProgram.log() << endl << endl;
+    }
+    texProgram.bindFragmentOutput("outColor");
+    vShader.free();
+    fShader.free();
+}
