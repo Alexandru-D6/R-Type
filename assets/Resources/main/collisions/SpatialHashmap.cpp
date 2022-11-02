5d6aa6f382c01ba9771fda4f0f2464789be684b3
Alexandru Dumitru <alexandru666@outlook.es>
2022-11-02 17:49:53 +0100

    cleaning up the code

--- assets/Resources/main/collisions/SpatialHashmap.cpp
+++ assets/Resources/main/collisions/SpatialHashmap.cpp
@@ -9,10 +9,10 @@ SpatialHashmap::~SpatialHashmap() {}
 void SpatialHashmap::insertObject(Collision* a) {
     glm::ivec4 cells = a->colliderBox;
 
-    cells.x += a->position.x + tileMapPos;
-    cells.y += a->position.y;
-    cells.z += a->position.x + tileMapPos;
-    cells.w += a->position.y;
+    cells.x += (int)(a->position.x + tileMapPos);
+    cells.y += (int)(a->position.y);
+    cells.z += (int)(a->position.x + tileMapPos);
+    cells.w += (int)(a->position.y);
 
     cells.x /= hashSize;
     cells.z /= hashSize;
@@ -31,10 +31,10 @@ void SpatialHashmap::insertObject(Collision* a) {
 void SpatialHashmap::insertObject(Collision* a, const glm::vec2 &pos) {
 	glm::ivec4 cells = a->colliderBox;
 
-    cells.x += pos.x + tileMapPos;
-    cells.y += pos.y;
-    cells.z += pos.x + tileMapPos;
-    cells.w += pos.y;
+    cells.x += (int)(pos.x + tileMapPos);
+    cells.y += (int)(pos.y);
+    cells.z += (int)(pos.x + tileMapPos);
+    cells.w += (int)(pos.y);
 
     cells.x /= hashSize;
     cells.z /= hashSize;
@@ -53,11 +53,6 @@ void SpatialHashmap::insertObject(Collision* a, const glm::vec2 &pos) {
 void SpatialHashmap::removeObject(Collision* a) {
 	for (int i = a->cells.x; i <= a->cells.z; ++i) {
 		for (int j = a->cells.y; j <= a->cells.w; ++j) {
-            stringstream stream;
-            string index;
-
-            stream << i << '.' << j;
-            stream >> index;
 
 			Hashmap[i][j][a->collisionGroup].erase(a);
 
@@ -71,10 +66,10 @@ void SpatialHashmap::updateObject(Collision* a, const glm::vec2 &newPos) {
     //return;
     glm::ivec4 boundingBox2 = a->colliderBox;
 
-    boundingBox2.x += newPos.x + tileMapPos;
-    boundingBox2.y += newPos.y;
-    boundingBox2.z += newPos.x + tileMapPos;
-    boundingBox2.w += newPos.y;
+    boundingBox2.x += (int)(newPos.x + tileMapPos);
+    boundingBox2.y += (int)(newPos.y);
+    boundingBox2.z += (int)(newPos.x + tileMapPos);
+    boundingBox2.w += (int)(newPos.y);
 
     boundingBox2.x /= hashSize;
     boundingBox2.z /= hashSize;
@@ -104,8 +99,8 @@ set<Collision*> SpatialHashmap::getNearByObjects(const glm::vec2 &pos, const int
     cells.y /= hashSize;
     cells.w /= hashSize;
 
-	for (int i = cells.x; i <= cells.z; ++i) {
-		for (int j = cells.y; j <= cells.w; ++j) {
+	for (int i = (int)cells.x; i <= (int)cells.z; ++i) {
+		for (int j = (int)cells.y; j <= (int)cells.w; ++j) {
             for (int t = 0; t < 10; ++t) {
                 if (groups[t]) {
                     result.insert(Hashmap[i][j][t].begin(), Hashmap[i][j][t].end());
